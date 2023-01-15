19de39d12bd1948441445678f8b4ef87229ea42b
Norain <mohnorain@gmail.com>
2023-01-15 13:01:38 -0600

    Added bitwise example code

---
+++ Jan-15_Bit_Manipulation/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+
+int main(){
+    //0000000000000000000010000111
+    //1111111111111111111101111000
+    uint8_t a =121;
+
+    //00000101
+    uint8_t b= 5;
+
+    std::cout<<(int)a<<std::endl;
+
+    return 0; 
+
+}
