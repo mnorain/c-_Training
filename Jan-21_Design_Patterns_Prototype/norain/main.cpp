aabcb45df9fc78d3aed8adb6a4d0dbbe80d15ee3
MohammedELtayibAjibJaber <119796996+MohammedELtayibAjibJaber@users.noreply.github.com>
2023-01-28 20:08:18 +0200

    Add files via upload

--- Jan-21_Design_Patterns_Prototype/norain/main.cpp
+++ Jan-21_Design_Patterns_Prototype/norain/main.cpp
@@ -1,53 +1,62 @@
-#include<iostream>
-class Address{
-    public:
-    std::string country;
-    std::string town;
-    std::string state;
-    int street;
+#include <iostream>
 
-    
-    Address(std::string country, std::string state, std::string town, int street):country(country),
-    town(town), state(state), street(street){}
+using namespace std;
 
-    void print(void){
-        std::cout<<"Country: "<<country<<" State: "<<state<<" Town: "<<town<<" Street: "<<street<<std::endl;
+class univAddress{
+public:
+    string country; 
+    string state;
+    string Locality;
+    string square;
+    int year;
+
+
+    univAddress (string country, string state,string Locality, string square, int year):country(country),state\
+    (state),Locality(Locality),square(square),year(year){}
+
+    void print (void){
+
+        cout<<"Country is "<<country<<" state is "<<state<<"  Locality is "<<Locality<<" square is "<<square <<\
+        " year is "<<year << endl;
+        
+
     }
 };
 
-class Student{
+class student{
     public:
-    std::string name;
-    std::string university;
-    std::string department;
-    std::string nationality;
-    int year_of_addmission;
-    Address* address;
-    
-    Student(std::string name, std::string university, std::string department, std::string nationality \
-    , int year_of_addmission, Address* address):name(name), university(university), department(department),\
-    nationality(nationality), year_of_addmission(year_of_addmission), address(address){}
+    string name;
+    string department;
+    string nationality;
+    int admissionYear;
+    univAddress* address;
 
-    Student(const Student& other_student):name(other_student.name), university(other_student.university), \
-    department(other_student.department), nationality(other_student.nationality), \
-    year_of_addmission(other_student.year_of_addmission), address(new Address{other_student.address->country,\
-    other_student.address->state,other_student.address->town, other_student.address->street}){}
+    student (string name, string department,string nationality, int admissionYear, univAddress address):name(name),\
+    department(department),nationality(nationality),admissionYear(admissionYear),address(& address){}
+        //{address.country,address.state\
+    , address.Locality,address.square,address.year}) {}//
+    student(const student& newstudent):name(newstudent.name),department(newstudent.department),nationality(newstudent.nationality),\
+    admissionYear(newstudent.admissionYear), address ( new univAddress {newstudent.address->country, newstudent.address->state\
+    ,newstudent.address->Locality,newstudent.address->square,newstudent.address->year}) {}
 
-    void print(void){
-        std::cout<<"Name: "<<name<<"University: "<<university<<" Department: "<<department<<" Nationality: "<<nationality<<\
-        " Year of addmission: "<<year_of_addmission<<" Address: ";
+    void print (void){
+
+        cout<<" student name  "<<name<<"  department "<<department<<"  nationality "<<nationality\
+        <<" addmissionYeare  "<< admissionYear<<" \n ";
+
         address->print();
     }
-};
-int main(){
-    Student tamir("Tamir Abdelgadir ", "UofK", "Electrical", "Sudanese", 2000, new Address{"Sudan", "Khartoum", "Jabra", 56});
-    Student taha= tamir;
-    taha.name= "Taha Yousif";
-    taha.address->town = "Omdurman";
-    taha.address->street = 23;
-    taha.print();
-    std::cout<<std::endl;
-    tamir.print();
 
+};
+
+int main(){
+
+    student tamir("Tamir omer","Electrical","Sudanese",2000, *new univAddress ("Sudan","khartoum","Khartoum","Sq 23",1898));
+    //tamir.print();
+    student Taha= tamir;
+    Taha.name=("Taha Yousif");
+    Taha.address->country = ("Eqypt");
+    Taha.print();
+    tamir.print();
     return 0;
 }
